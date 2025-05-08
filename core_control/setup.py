5db605439affd5a5cf5480f2e8b9ca96509a2ae0
Steven <steven41@ui.ac.id>
2025-05-08 17:01:13 +0700

    modified setup.py for asv

--- core_control/setup.py
+++ core_control/setup.py
@@ -22,7 +22,7 @@ setup(
         'console_scripts': [
             'motor_controller = core_control.motor_controller:main',
             'microcontroller_rov = core_control.microcontroller_rov:main',
-            'microcontroller_rov = core_control.microcontroller_asv:main'
+            'microcontroller_asv = core_control.microcontroller_asv:main'
         ],
     },
 )
