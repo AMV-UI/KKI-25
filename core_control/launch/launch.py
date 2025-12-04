4e525502b3462153e1ff9e07cb2798c472532f60
coolcmyk <75294231+coolcmyk@users.noreply.github.com>
2025-12-04 11:13:09 +0700

    dev

--- core_control/launch/launch.py
+++ core_control/launch/launch.py
@@ -8,36 +8,36 @@ from launch_ros.actions import Node
 def generate_launch_description():
 
     return LaunchDescription([
-        Node(
-            package="core_control",
-            executable="pixhawk_controller",
-            name="pixhawk_controller",
-            output="screen",
-        ),
-        Node(
-            package="core_control",
-            executable="esp_controller",
-            name="esp_controller",
-            output="screen",
-        ),
-        Node(
-            package="core_control",
-            executable="pwm_controller",
-            name="pwm_controller",
-            output="screen",
-        ),
-        Node(
-            package="core_control",
-            executable="motor_controller",
-            name="motor_controller",
-            output="screen",
-        ),
         # Node(
         #     package="core_control",
-        #     executable="movement_simulator",
-        #     name="movement_simulator",
+        #     executable="pixhawk_controller",
+        #     name="pixhawk_controller",
         #     output="screen",
         # ),
+        # Node(
+        #     package="core_control",
+        #     executable="esp_controller",
+        #     name="esp_controller",
+        #     output="screen",
+        # ),
+        # Node(
+        #     package="core_control",
+        #     executable="pwm_controller",
+        #     name="pwm_controller",
+        #     output="screen",
+        # ),
+        # Node(
+        #     package="core_control",
+        #     executable="motor_controller",
+        #     name="motor_controller",
+        #     output="screen",
+        # ),
+        Node(
+            package="core_control",
+            executable="movement_simulator",
+            name="movement_simulator",
+            output="screen",
+        ),
         # Node(
         #     package="core_control",
         #     executable="channel_simulator",
