913cfc1241d77dcc274683c03bafcfb0ac032d19
apenchuu <steven41@ui.ac.id>
2026-06-23 15:28:15 +0700

    area < 200

--- core/core/perception/image/inference.py
+++ core/core/perception/image/inference.py
@@ -121,7 +121,7 @@ class ObjectDetector:
                     area = abs((x2 - x1) * (y2 - y1))
 
                     # Filter out small/far away objects
-                    if area < 600:
+                    if area < 200:
                         continue
 
                     if mission == MissionStatus.BUOY:
