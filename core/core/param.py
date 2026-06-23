d9bced68a2446a830c090d62a34d800eb29c9055
amvop <amvuijuara@gmail.com>
2026-06-23 14:03:21 +0700

    fix issue di param.py

--- core/core/param.py
+++ core/core/param.py
@@ -6,13 +6,11 @@ from core.utils.config import Topic, MissionStatus
 from time import sleep
 
 class ParameterBlackboard(Node):    
-    def __init__(self, node = Node):
-        super().__init__(
-            'parameter_blackboard',
-        )
+    def __init__(self):
+        # Initialize this class as an rclpy Node
+        super().__init__('parameter_blackboard')
 
-        self.node = node
-
+        # Use self as the node for publishers/subscribers
         self.arena = "B"
         self.st_speed = 1.0
         self.tn_speed = 1.0
@@ -26,35 +24,27 @@ class ParameterBlackboard(Node):
         # Important: delete === gay
         # sleep(5.0)# Wait for other nodes to be ready
         # Create publishers and retain references so they aren't garbage collected
-        self.arena_pub = Topic.arena.createPublisher(self.node)
-        self.st_speed_pub = Topic.st_speed.createPublisher(self.node)
-        self.tn_speed_pub = Topic.tn_speed.createPublisher(self.node)
-        self.mission_type_pub = Topic.mission_type.createPublisher(self.node)
-        
+        self.arena_pub = Topic.arena.createPublisher(self)
+        self.st_speed_pub = Topic.st_speed.createPublisher(self)
+        self.tn_speed_pub = Topic.tn_speed.createPublisher(self)
+        self.mission_type_pub = Topic.mission_type.createPublisher(self)
+
         # Now publish
         self.arena_pub.publish(String(data=self.arena))
         self.st_speed_pub.publish(Float64(data=self.st_speed))
         self.tn_speed_pub.publish(Float64(data=self.tn_speed))
         self.mission_type_pub.publish(String(data=self.mission_type))
-        
-        self.node.get_logger().info(f'<> [ParameterBlackboard] Published initial parameters {self.arena}, {self.st_speed}, {self.tn_speed}, {self.dock_lat}, {self.dock_lon}, {self.mission_type}')
 
-        self.arena_sub = Topic.arena.createSubscriber(
-            self,
-            self._arena_cb
+        self.get_logger().info(
+            f'<> [ParameterBlackboard] Published initial parameters '
+            f'{self.arena}, {self.st_speed}, {self.tn_speed}, '
+            f'{self.dock_lat}, {self.dock_lon}, {self.mission_type}'
         )
-        self.st_speed_sub = Topic.st_speed.createSubscriber(
-            self,
-            self._st_speed_cb
-        )
-        self.tn_speed_sub = Topic.tn_speed.createSubscriber(
-            self,
-            self._tn_speed_cb
-        )
-        self.mission_type_sub = Topic.mission_type.createSubscriber(
-            self,
-            self._mission_type_cb
-        )
+
+        self.arena_sub = Topic.arena.createSubscriber(self, self._arena_cb)
+        self.st_speed_sub = Topic.st_speed.createSubscriber(self, self._st_speed_cb)
+        self.tn_speed_sub = Topic.tn_speed.createSubscriber(self, self._tn_speed_cb)
+        self.mission_type_sub = Topic.mission_type.createSubscriber(self, self._mission_type_cb)
     
     def _mission_type_cb(self, msg: String):
         self.mission_type = msg.data
@@ -70,7 +60,7 @@ class ParameterBlackboard(Node):
     
 def main(args=None):
     rclpy.init(args=args)
-    node = ParameterBlackboard(rclpy.create_node('param'))
+    node = ParameterBlackboard()
     
     try:
         node.get_logger().info(f'Spinning... Param')
