906617c5d02e5b8c642c2551a598da710b20d8e7
amvop <amvuijuara@gmail.com>
2026-06-23 16:19:26 +0700

    change value box

--- core/core/perception/image/inference.py
+++ core/core/perception/image/inference.py
@@ -121,7 +121,7 @@ class ObjectDetector:
                     area = abs((x2 - x1) * (y2 - y1))
 
                     # Filter out small/far away objects
-                    if area < 200:
+                    if area < 400:
                         continue
 
                     if mission == MissionStatus.BUOY:
