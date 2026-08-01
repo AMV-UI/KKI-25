2f482dabc8fb7529858bda2508bef1dbdacdd102
apenchuu <steven41@ui.ac.id>
2026-08-01 16:56:06 +0700

    finding bener di real2

--- core_behavior_tree/core_behavior_tree/behaviors/mission/actions/mission/finding.py
+++ core_behavior_tree/core_behavior_tree/behaviors/mission/actions/mission/finding.py
@@ -76,7 +76,7 @@ class Finding_Execution(BaseExecution):
 
         # Reverse yaw effort to counter Turn_Next_Buoy overshoot
         # Arena A: Turn Left (Negative), Arena B: Turn Right (Positive)
-        yaw_val = float(self.effort * (1 if self.arena == "A" else -1))
+        yaw_val = float(self.effort * (-1 if self.arena == "A" else 1))
         self.yaw_effort_pub.publish(Float64(data=yaw_val))
         
         # Zero speed effort during finding phase, just yaw
