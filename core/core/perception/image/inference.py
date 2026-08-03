3a58925e25cac1520ec1cae9ba11e23747bbd416
apenchuu <steven41@ui.ac.id>
2026-08-03 17:11:39 +0700

    change speed and pixel

--- core/core/perception/image/inference.py
+++ core/core/perception/image/inference.py
@@ -124,7 +124,7 @@ class ObjectDetector:
 
                     # Filter out small/far away objects
                     # if area < 600: #simulasi
-                    if area < 400:
+                    if area < 200:
                         continue
 
                     if mission == MissionStatus.BUOY:
--- core/core/utils/config.py
+++ core/core/utils/config.py
@@ -39,23 +39,23 @@ class MissionStatus:
 
 class MissionParams:
     # Buoy Mission
-    buoy_speed_effort = 120.0
+    buoy_speed_effort = 150.0
     buoy_yaw_effort = 200.0
     buoy_time_threshold = 2.0
 
     # Turn Next Buoy Mission
-    turn_next_buoy_speed_effort = 120.0
+    turn_next_buoy_speed_effort = 150.0
     turn_next_buoy_yaw_effort = 150.0
     turn_next_buoy_duration = 10.0
 
     # Finding Mission BOX
-    finding_speed_effort = 120.0
+    finding_speed_effort = 150.0
     finding_yaw_effort = 150.0
     finding_time_threshold = 0.2
     finding_approach_duration = 5.0
 
     # Unfinding Mission BOX
-    unfinding_speed_effort = 120.0
+    unfinding_speed_effort = 150.0
     unfinding_yaw_effort = 150.0
     unfinding_time_threshold = 0.2
     unfinding_duration = 5.0
