d9a7f62410735458411a3d41ac9618adfb73533f
apenchuu <steven41@ui.ac.id>
2026-08-03 16:27:08 +0700

    change turn next buoy

--- core/core/utils/config.py
+++ core/core/utils/config.py
@@ -46,7 +46,7 @@ class MissionParams:
     # Turn Next Buoy Mission
     turn_next_buoy_speed_effort = 120.0
     turn_next_buoy_yaw_effort = 150.0
-    turn_next_buoy_duration = 5.0
+    turn_next_buoy_duration = 10.0
 
     # Finding Mission BOX
     finding_speed_effort = 120.0
